0d6889135e210c3c2b8c2996418c6d8a7a298186
droomek <rafalopalanko@gmail.com>
2018-03-14 22:41:29 +0100

    Poprawa pakietów

--- src/com/droomek/Vehicle.java
+++ src/com/droomek/Vehicle.java
@@ -1,6 +1,5 @@
 package com.droomek;
 
-import com.sun.xml.internal.bind.v2.TODO;
 
 import java.util.Scanner;
 import java.util.concurrent.TimeUnit;
@@ -48,7 +47,7 @@ public class Vehicle {
         }
     }
 
-    public void vehicleSpeed(String[] vehicleSpeed) {
+    void vehicleSpeed(String[] vehicleSpeed) {
         boolean flag = true;
         int speed = 0;
         Scanner scanner = new Scanner ( System.in );
@@ -57,35 +56,37 @@ public class Vehicle {
             System.out.println ( "Faster write \"+\" \n" +
                     "Slower write \"-\" " );
             String sign = scanner.nextLine ();
-            if (sign.equals ( "+" )) {
-                if (speed == 0 || speed <5){
-                speed++;
-                System.out.println ( "Current speed " + vehicleSpeed[speed] + "km/h." );
-                }
-                else
-                    System.out.println ( vehicleSpeed [speed] + "km/h is the highest speed.");
-            } else if (sign.equals ( "-" )){
-                if (speed !=0) {
-                    speed--;
-                    System.out.println ( "Current speed " + vehicleSpeed[speed] + "km/h." );
-                }else {
-                    System.out.println ( "Current speed " + vehicleSpeed[speed] + "km/h." );
-                    flag = false;
-                }
-            }
-            else{
-                System.out.println ("Incorrect value");
+            switch (sign) {
+                case "+":
+                    if (speed == 0 || speed < 5) {
+                        speed++;
+                        System.out.println ( "Current speed " + vehicleSpeed[speed] + "km/h." );
+                    } else
+                        System.out.println ( vehicleSpeed[speed] + "km/h is the highest speed." );
+                    break;
+                case "-":
+                    if (speed != 0) {
+                        speed--;
+                        System.out.println ( "Current speed " + vehicleSpeed[speed] + "km/h." );
+                    } else {
+                        System.out.println ( "Current speed " + vehicleSpeed[speed] + "km/h." );
+                        flag = false;
+                    }
+                    break;
+                default:
+                    System.out.println ( "Incorrect value" );
+                    break;
             }
 
         }
     }
 
 
-    public String getName() {
+    String getName() {
         return name;
     }
 
-    public String getModelName() {
+    String getModelName() {
         return modelName;
     }
 }
