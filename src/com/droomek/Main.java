6dde3469c0a0f759b8404db38c0c3cd23022d9a4
droomek <rafalopalanko@gmail.com>
2018-03-17 23:17:04 +0100

    Revise of code

--- src/com/droomek/Main.java
+++ src/com/droomek/Main.java
@@ -5,9 +5,11 @@ public class Main {
     public static void main(String[] args) throws InterruptedException {
 
         Vehicle vehicle1 = new Vehicle("Furmanka");
+        Vehicle vehicle2 = new Vehicle ( "Hulajnoga" );
         Bike bike1 = new Bike ( "Romet","Wigry 4" );
         Car car1 = new Car ( "Mitsubishi", "ASX","1,6" );
         Plane plane1 = new Plane ( "Boening","777" );
+        Plane plane2 = new Plane ("Boening", "JumboJet");
 
 
         //vehicle1.start ();
@@ -16,7 +18,12 @@ public class Main {
         //bike1.stop ();
         //car1.start ();
         //car1.stop ();
-        plane1.start ();
-        plane1.stop ();
+        //plane1.start ();
+        //plane1.stop ();
+        //vehicle2.start ();
+        //vehicle2.stop ();
+        plane2.start ();
+        plane2.stop ();
+
     }
 }
